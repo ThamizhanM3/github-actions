79ebec583d5b347702828a43ec95a73e9a505d1a
Thamizhan M3 <mithelesh.tmz.m3@gmail.com>
2026-04-11 10:14:12 +0530

    task 06 v1

---
+++ src/main/java/App.java
@@ -0,0 +1,9 @@
+public class App {
+    public static void main(String[] args) {
+        System.out.println("Hello, GitHub Actions!");
+    }
+
+    public static int add(int a, int b) {
+        return a + b;
+    }
+}
